b7b37f3807133a80fe659c7d00a6d0fdb702ef85
Qing-Qiu <91710069+Qing-Qiu@users.noreply.github.com>
2022-01-28 17:53:35 +0800

    Add files via upload

---
+++ takeout_bin/src/com/takeout/login/AdminChangePwd.java
@@ -0,0 +1,31 @@
+package com.takeout.login;
+
+import java.sql.*;
+
+public class AdminChangePwd {
+    public static void ChangePwd(Connection conn, String oldPwd, String newPwd) {
+        try {
+            String sql1 = "select * from admin";
+            PreparedStatement psql1 = conn.prepareStatement(sql1);
+            ResultSet re = psql1.executeQuery();
+            re.next();
+            String password = re.getString("password");
+            if (password.equals(oldPwd)) {
+                String sql2 = "update admin set password =? where id =?";
+                PreparedStatement psql2 = conn.prepareStatement(sql2);
+                psql2.setObject(1, newPwd);
+                psql2.executeUpdate();
+                System.out.println("密码更改成功！\n您的新密码是 " + newPwd);
+                psql2.close();
+            } else {
+                System.out.println("原密码错误，请重新输入！");
+            }
+            re.close();
+            psql1.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
